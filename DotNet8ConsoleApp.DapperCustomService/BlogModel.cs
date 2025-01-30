e21906b0d746dc8c4b735002eb828512d5bba2ac
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:25:17 +0630

    Format Code

--- DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
+++ DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8ConsoleApp.DapperCustomService
+﻿namespace DotNet8ConsoleApp.DapperCustomService;
+
+public class BlogModel
 {
-	public class BlogModel
-	{
-		public long BlogId { get; set; }
-		public string? BlogTitle { get; set; }
-		public string? BlogAuthor { get; set; }
-		public string? BlogContent { get; set; }
-		public bool DeleteFlag { get; set; }
-	}
+	public long BlogId { get; set; }
+	public string? BlogTitle { get; set; }
+	public string? BlogAuthor { get; set; }
+	public string? BlogContent { get; set; }
+	public bool DeleteFlag { get; set; }
 }
