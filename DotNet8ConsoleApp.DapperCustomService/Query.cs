06da07683d71e9a4825338023bf66fa1d95eb687
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:29:22 +0630

    Format Code

--- DotNet8ConsoleApp.DapperCustomService/Query.cs
+++ DotNet8ConsoleApp.DapperCustomService/Query.cs
@@ -1,30 +1,29 @@
-﻿namespace DotNet8ConsoleApp.DapperCustomService
+﻿namespace DotNet8ConsoleApp.DapperCustomService;
+
+public class Query
 {
-	public class Query
-	{
-		public static string GetAllBlogsQuery { get; } =
-		@"SELECT [BlogId]
+	public static string GetAllBlogsQuery { get; } =
+	@"SELECT [BlogId]
       ,[BlogTitle]
       ,[BlogAuthor]
       ,[BlogContent]
       ,[DeleteFlag]
   FROM [dbo].[Tbl_Blog] ORDER BY BlogId DESC";
 
-		public static string CreateBlogQuery { get; } =
-		"INSERT INTO dbo.Tbl_Blog (BlogTitle, BlogAuthor, BlogContent, DeleteFlag)" +
-			" VALUES (@BlogTitle, @BlogAuthor, @BlogContent, @DeleteFlag);";
+	public static string CreateBlogQuery { get; } =
+	"INSERT INTO dbo.Tbl_Blog (BlogTitle, BlogAuthor, BlogContent, DeleteFlag)" +
+		" VALUES (@BlogTitle, @BlogAuthor, @BlogContent, @DeleteFlag);";
 
-		public static string UpdateBlogQuery { get; } =
-		@"UPDATE dbo.Tbl_Blog 
+	public static string UpdateBlogQuery { get; } =
+	@"UPDATE dbo.Tbl_Blog 
 	  SET BlogTitle = @BlogTitle, 
 	      BlogAuthor = @BlogAuthor, 
 	      BlogContent = @BlogContent 
 	  WHERE BlogId = @BlogId";
 
-		public static string DeleteBlogQuery { get; } =
-		@"UPDATE dbo.Tbl_Blog 
+	public static string DeleteBlogQuery { get; } =
+	@"UPDATE dbo.Tbl_Blog 
 		  SET DeleteFlag = 1 
 		  WHERE BlogId = @BlogId";
 
-	}
 }
