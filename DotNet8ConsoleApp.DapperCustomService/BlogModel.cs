2f8725e79d74ed45ab9367a30aa7d98be956c178
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:25:38 +0630

    -

--- DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
+++ DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace DotNet8ConsoleApp.DapperCustomService;
 
+#region Blog Model
+
 public class BlogModel
 {
 	public long BlogId { get; set; }
@@ -8,3 +10,5 @@ public class BlogModel
 	public string? BlogContent { get; set; }
 	public bool DeleteFlag { get; set; }
 }
+
+#endregion
