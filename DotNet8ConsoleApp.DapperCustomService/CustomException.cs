8d1a5e00f18adcd351596cd5db7cca2fc581a946
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:23:42 +0630

    -

--- DotNet8ConsoleApp.DapperCustomService/CustomException.cs
+++ DotNet8ConsoleApp.DapperCustomService/CustomException.cs
@@ -1,6 +1,10 @@
 ﻿namespace DotNet8ConsoleApp.DapperCustomService;
 
+#region CustomException
+
 public class CustomException : Exception
 {
 	public CustomException(string? message) : base(message) { }
 }
+
+#endregion
