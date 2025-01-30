6b69c87087cf66657b3c73ee7c34d004c75ee482
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:33:40 +0630

    -

--- DotNet8ConsoleApp.DapperCustomService/GlobalUsing.cs
+++ DotNet8ConsoleApp.DapperCustomService/GlobalUsing.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8ConsoleApp.DapperCustomService
-{
-	internal class GlobalUsing
-	{
-	}
-}
+﻿
