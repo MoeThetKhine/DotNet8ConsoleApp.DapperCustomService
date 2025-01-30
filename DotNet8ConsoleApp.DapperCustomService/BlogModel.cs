57f6ef1dd98ea9bf3528949bbbde7ff990f3958d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-30 20:24:50 +0630

    Feat : Add Blog Model

--- DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
+++ DotNet8ConsoleApp.DapperCustomService/BlogModel.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace DotNet8ConsoleApp.DapperCustomService
 {
-	internal class BlogModel
+	public class BlogModel
 	{
+		public long BlogId { get; set; }
+		public string? BlogTitle { get; set; }
+		public string? BlogAuthor { get; set; }
+		public string? BlogContent { get; set; }
+		public bool DeleteFlag { get; set; }
 	}
 }
