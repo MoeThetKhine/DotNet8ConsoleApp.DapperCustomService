a99898365e441190c652f65add77fcac5bff6090
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 15:02:23 +0630

    Feat : Add Query

--- DotNet8ConsoleApp.DapperCustomService/DapperService.cs
+++ DotNet8ConsoleApp.DapperCustomService/DapperService.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNet8ConsoleApp.DapperCustomService
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace DotNet8ConsoleApp.DapperCustomService
 {
-	internal class DapperService
+	public class DapperService
 	{
+		private readonly string _connectionString;
+
+		public DapperService(string connectionString)
+		{
+			_connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+		}
+		private IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+		public List<T> Query<T>(string query, object? parameters = null)
+		{
+			using var db = CreateConnection();
+			return db.Query<T>(query, parameters).ToList();
+		}
 	}
 }
