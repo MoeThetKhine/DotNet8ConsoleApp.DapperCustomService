b0696f12e9ada03af4c5245eb979ec08decb262b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-31 15:01:55 +0630

    Feat : Add Connection String

--- DotNet8ConsoleApp.DapperCustomService/Program.cs
+++ DotNet8ConsoleApp.DapperCustomService/Program.cs
@@ -5,4 +5,5 @@ public class Program
 	private static readonly string _connectionString = "Data Source=.;Database=DotNetTrainingBatch5;User Id=sa;Password=sasa@123;TrustServerCertificate=True;";
 
 	private static readonly DapperService _dapperService = new(_connectionString);
+
 }
